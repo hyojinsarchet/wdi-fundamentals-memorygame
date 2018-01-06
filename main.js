aff1f7d3912c85e336571fc77c7d71fe3fbf7252
hyojin.sarchet <hyojin5931@hanmail.net>
2018-01-05 19:38:10 -0500

    created game logic

---
+++ main.js
@@ -0,0 +1,9 @@
+// Create cards for the memory game.
+
+var cardOne = "king";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "queen";
+
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardTwo);
