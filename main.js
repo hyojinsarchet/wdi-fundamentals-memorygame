577a00c76e9470e6a779313595e3485e64c1d8a0
hyojin.sarchet <hyojin5931@hanmail.net>
2018-01-11 21:47:17 -0500

    added dom manipulation and reset button

--- main.js
+++ main.js
@@ -22,24 +22,56 @@ var cards = [
   }
 ];
 
+
+// Clicked cards come to this array
 var cardsInPlay =[];
 
+
+// Function to show alert either they are match or not
 var checkForMatch = function() {
     if (cardsInPlay[0] === cardsInPlay[1]) {
-    alert("You found a match!");
+        alert("You found a match!");
     } else {
-    alert("Sorry, try again.");
+        alert("Sorry, try again.");
     }
-}
+};
 
-var flipCard = function(cardId) {
 
-    console.log("User flipped " + cards[cardId].rank);
-    console.log(cards[cardId].cardImage);
-    console.log(cards[cardId].suit);
+// Function to flip cards
+var flipCard = function() {
+    var cardId = this.getAttribute('data-id');
+    // console.log("User flipped " + cards[cardId].rank);
+    // console.log(cards[cardId].cardImage);
+    // console.log(cards[cardId].suit);
     cardsInPlay.push(cards[cardId].rank);
-    checkForMatch();
-}
+    this.setAttribute('src', cards[cardId].cardImage);
 
-flipCard(0);
-flipCard(2);
+    // When 2 cards are flipped call 'checkForMatch' function
+    if(cardsInPlay.length === 2) {
+        checkForMatch();
+    }
+};
+
+
+// Created main game board using DOM manipulation
+var createBoard = function() {
+    for(var i = 0; i < cards.length; i++) {
+        var cardElement = document.createElement('img');
+        cardElement.setAttribute('src', 'images/back.png');
+        cardElement.setAttribute('data-id', i);
+        cardElement.addEventListener('click', flipCard);
+        document.getElementById('game-board').appendChild(cardElement);
+    }
+};
+
+// Invoke the createBoard function
+createBoard();
+
+
+// When the reset button is clicked the the game is reset to play again
+document.getElementById("reset").onclick = function() {
+    document.getElementById("game-board").innerHTML = "";
+    createBoard();
+    cardsInPlay = [];
+};
+    
